562d66ff91aeca1690b36679e58f81010780b01f
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-23 23:36:51 +0100

    added webpack config file for both the main webpage and 404 page

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: './src/client/main.js',
+    _404: './src/client/404.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'public/static/'),
+    publicPath: '/',
+    filename: '[name].bundle.js'
+  },
+}
