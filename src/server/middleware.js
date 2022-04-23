cac7a90144dbbc0f6de2b667a60ea9a968110f0e
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-23 23:43:45 +0100

    added file responsible for setting up the express middleware

---
+++ src/server/middleware.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+
+const middleware = (app) => {
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: 'false' }))
+
+    const session_config = {
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false }
+    }
+    if (process.env.NODE_ENV === 'production') {
+        session_config.cookie.secure = true
+    }
+    app.use(session(session_config))
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use((req, res, next) => {
+            req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+        })
+    }
+
+    app.use(express.static(path.join(__dirname, '..', '..', 'public', 'static')))
+}
+
+module.exports = {
+    config: middleware
+}
