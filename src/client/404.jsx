16f18ab0271209ca0dc037e21ab59373a676a985
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-24 22:25:37 +0100

    added simple Navbar to the webpage

--- src/client/404.jsx
+++ src/client/404.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './404.css'
+import NavigBar from './NavigBar'
 
 class App extends Component {
   render(){
     return(
       <div className='_404'>
+        <NavigBar />
         <h1> Nothing to see here! 404! </h1>
       </div>
     )
--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import './app.css'
+import NavigBar from './NavigBar'
 
 class App extends Component {
   render(){
     return(
       <div className='App'>
+        <NavigBar />
         <h1> Hello, World! </h1>
       </div>
     )
