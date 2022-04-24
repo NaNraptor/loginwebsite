ef47dcded3bed25bbb24d15db8b4c595899a78d3
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-24 22:24:48 +0100

    added bootstrap and updated syntax to react 18

--- src/client/404.js
+++ src/client/404.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import _404 from './404.jsx'
 
-ReactDOM.render(<_404 />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <_404 />
+  </React.StrictMode>
+)
