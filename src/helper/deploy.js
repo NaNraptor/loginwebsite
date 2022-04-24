10121d17873a3e82445851a3e3c0cb45fae6436b
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-24 22:22:49 +0100

    added additional checks for folder existense in the deployment script

--- src/helper/deploy.js
+++ src/helper/deploy.js
@@ -1,6 +1,20 @@
+require('dotenv').config()
+
 const fs = require('fs')
 const path = require('path')
 const logger = require('./logger').config('client-deploy-script')
+
+if (!process.env.PUBLIC_FOLDER) {
+    logger.error('PUBLIC_FOLDER env variable not specified')
+    logger.error('Could not deploy client facing code')
+    process.exit(-1)
+}
+
+if (!fs.existsSync(process.env.PUBLIC_FOLDER)) {
+    logger.warning(`${process.env.PUBLIC_FOLDER} does not exist, creating it now`)
+
+    fs.mkdirSync(path.join(process.env.PUBLIC_FOLDER, 'static'), { recursive: true })
+}
 
 const ignored = ['main.js', '404.js']
 
@@ -9,7 +23,7 @@ fs.readdirSync('./src/client').forEach(file => {
     if (file.endsWith('.jsx')) return
 
     const old_path = path.join(__dirname, '..', 'client', file)
-    const new_path = path.join(__dirname, '..', '..', 'public', file.endsWith('.html') ? '' : 'static', file)
+    const new_path = path.join(process.env.PUBLIC_FOLDER, file.endsWith('.html') ? '' : 'static', file)
 
     logger.debug(`old_path: ${old_path}`)
     logger.debug(`new_path: ${new_path}`)
@@ -17,7 +31,7 @@ fs.readdirSync('./src/client').forEach(file => {
     fs.copyFile(old_path, new_path, (err) => {
         if (err) {
             logger.error('Could not deploy client facing code')
-            return
+            process.exit(-1)
         }
         logger.debug(`Successfully deployed ${file}`)
     })
