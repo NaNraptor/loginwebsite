389cbfbc51e2d938096372ccb1b83c2c57900d38
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-25 21:20:10 +0100

    fixed unsupported import and root creation

--- src/client/404.js
+++ src/client/404.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import _404 from './404.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <_404 />
