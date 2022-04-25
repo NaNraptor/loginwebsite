e8df70e171a445b95d826ba2841cbec7620c7ebf
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-25 21:22:57 +0100

    added a common utils client side js file
    it will function to make fetch requests as well as other common activities

---
+++ src/client/utils.js
@@ -0,0 +1,54 @@
+export const signup = async (data) => {
+    if (data.username === '' ||
+        data.password === '' ||
+        data.first_name === '' ||
+        data.second_name === '' ||
+        data.email === '' ||
+        data.post_code === '' ||
+        data.additional === '') {
+        return false
+    }
+
+    const rawResponse = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    return await rawResponse.json()
+}
+
+export const login = async (data) => {
+    if (data.username === '' ||
+        data.password === '') {
+        return false
+    }
+
+    const rawResponse = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    return await rawResponse.json()
+}
+
+const getCookie = (name) => {
+    const value = `; ${document.cookie}`
+    const parts = value.split(`; ${name}=`)
+    if (parts.length === 2) return parts.pop().split(';').shift()
+}
+
+export const isLoggedIn = () => {
+    return getCookie('username') === '' ? false : true
+}
+
+export const getLoggedInUser = () => {
+    return getCookie('username')
+}
