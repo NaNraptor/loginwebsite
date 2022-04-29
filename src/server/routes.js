18646e4953425681d7414c42df163e876c2f07f2
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-04-29 16:59:42 +0100

    added minor logging improvements  as well as a login route with the correct middleware

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+const logger = require('../helper/logger').config('routes')
+const route_funcs = require('./route-funcs')
+
 const route = (app) => {
     app.get('/', (req, res) => {
         res.sendFile(path.join(process.env.PUBLIC_FOLDER, 'index.html'))
@@ -10,11 +13,19 @@ const route = (app) => {
         
     })
 
-    app.post('/login', (req, res) => {
-        
-    })
+    //A timing attack can be used to check if username exists in the DB
+    //Might fix later, probably not
+    //DoS attack also possible if bcrypt is extended to pwds of >70 bytes
+    //https://security.stackexchange.com/a/184090
+    app.post('/login',
+        route_funcs.setLoginStatus,
+        route_funcs.setAccessRank,
+        route_funcs.setBannedStatus,
+        route_funcs.routeEndSuccess
+    )
 
     app.get('*', (req, res) => {
+        logger.debug('404 page accessed')
         res.status(404).sendFile(path.join(process.env.PUBLIC_FOLDER, '404.html'))
     })
 }
