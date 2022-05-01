0987c7d67a7b2963ca48a0f05305fa76d7cd809e
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-02 00:28:47 +0100

    added basic view management functionality

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import NavigBar from './NavigBar'
 import Login from './Login'
+import Dashboard from './Dashboard'
+
+import * as utils from './utils'
 
 const App = () => {
+    const [logged_in, setLogged_in] = useState(utils.isLoggedIn())
+
+    const user_view = (
+        <Dashboard />
+    )
+
+    const login_view = (
+        <Login setLogged_in={setLogged_in}/>
+    )
+
+    let current_view = login_view
+
+    if (logged_in) {
+        current_view = user_view
+    } else {
+        current_view = login_view
+    }
+
     return (
         <div className='App'>
-            <NavigBar />
-            <Login />
+            <NavigBar setLogged_in={setLogged_in}/>
+            {current_view}
         </div>
     )
 }
