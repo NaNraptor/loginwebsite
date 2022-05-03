177ce1ec3fd951024af3aa6b9e4a0bce8c6047d3
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-03 23:48:28 +0100

    fixed some linting errors

--- src/client/Login.jsx
+++ src/client/Login.jsx
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col'
 import Alert from 'react-bootstrap/Alert'
 
 import * as utils from './utils'
+import Views from './views'
 
 const Login = (props) => {
     const [username, setUsername] = useState('')
@@ -21,20 +22,17 @@ const Login = (props) => {
         const result = await utils.login({ username: username, password: password })
 
         if (!result.success) {
-            setLogin_alert(<Alert className="mt-1" key={ 'danger' } variant={ 'danger' }> Log in failed. Please try again. </Alert>)
-        } else {
-            setLogin_alert(<Alert className="mt-1" key={ 'success' } variant={ 'success' }> Log in successful. Please await redirect. </Alert>)
-            props.setLogged_in(true)
+            return setLogin_alert(<Alert className='mt-1' key={ 'danger' } variant={ 'danger' }> Log in failed. Please try again. </Alert>)
         }
     }
 
     return (
         <Row>
             <Col />
-            <Col className="justify-content-md-center" sm={4} md="auto">
-                <Form onSubmit={handleSubmit} className="d-grid">
-                    <Form.Group className="mb-3" controlId='username'>
-                        <Form.Label className="font-weight-bold">Username</Form.Label>
+            <Col className='justify-content-md-center' sm={4} md='auto'>
+                <Form onSubmit={handleSubmit} className='d-grid'>
+                    <Form.Group className='mb-3' controlId='username'>
+                        <Form.Label className='font-weight-bold'>Username</Form.Label>
                         <Form.Control
                             autoFocus
                             type='username'
@@ -42,16 +40,16 @@ const Login = (props) => {
                             onChange={(e) => setUsername(e.target.value)}
                         />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId='password'>
-                        <Form.Label className="font-weight-bold">Password</Form.Label>
+                    <Form.Group className='mb-3' controlId='password'>
+                        <Form.Label className='font-weight-bold'>Password</Form.Label>
                         <Form.Control
                             type='password'
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId='rememberme'>
-                        <Form.Check type="checkbox" label="Remember me" onClick={(e) => utils.remember_me({ remember_me: e.target.checked })}/>
+                    <Form.Group className='mb-3' controlId='rememberme'>
+                        <Form.Check type='checkbox' label='Remember me' onClick={(e) => utils.remember_me({ remember_me: e.target.checked })}/>
                     </Form.Group>
                     <Button type='submit' disabled={!validateForm()}>
                     Login
