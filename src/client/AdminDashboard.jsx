1595e3d1c304e837a9812efa9cdbdcedc13c5ad4
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-03 23:44:49 +0100

    added a skeleton admindashboard page

---
+++ src/client/AdminDashboard.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+import * as utils from './utils'
+
+const AdminDashboard = () => {
+    return (
+        <div className='AdminDashboard'>
+            
+        </div>
+    )
+}
+
+export default AdminDashboard
