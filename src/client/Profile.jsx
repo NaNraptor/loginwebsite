04db6fc3edf52272e5250ea2ca274b110f068979
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-03 23:44:22 +0100

    created a skeleton profile page

---
+++ src/client/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+import * as utils from './utils'
+
+const Profile = () => {
+    return (
+        <div className='Profile'>
+            
+        </div>
+    )
+}
+
+export default Profile
