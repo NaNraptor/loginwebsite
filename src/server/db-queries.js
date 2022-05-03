ba290141a02a08ecad50c7141250f181cdce7355
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-03 23:45:12 +0100

    updated the banned sb query to correctly filter on user_id instead of the ban_id

--- src/server/db-queries.js
+++ src/server/db-queries.js
@@ -1,5 +1,5 @@
 module.exports = {
     login: 'SELECT "id", "password", "group" FROM "users" WHERE "username"=$1',
     user_group: 'SELECT "access_rank" FROM "groups" WHERE "id"=$1',
-    banned: 'SELECT "user_id", "reason" FROM "bans" WHERE "id"=$1'
+    banned: 'SELECT "reason" FROM "bans" WHERE "user_id"=$1'
 }
