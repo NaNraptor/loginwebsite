a5da4d85b00f9f460ea57ff6ec54597f7221bb48
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-04 16:46:04 +0100

    fixed indents

--- src/client/main.js
+++ src/client/main.js
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
