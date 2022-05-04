c260b9848ea4c12d3125ed435e77156eda2ae414
Nanyo Nanev <nanyo.nanev.jr@gmail.com>
2022-05-04 16:47:13 +0100

    added logged in checks to dashboard page

--- src/client/Dashboard.jsx
+++ src/client/Dashboard.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import * as utils from './utils'
+import Views from './views'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
+    useEffect(() => {
+        if (!utils.isLoggedIn()) {
+            return props.state.setCurrent_view(Views.login(props.state))
+        }
+    }, [])
+
     return (
         <div className='Dashboard'>
             
